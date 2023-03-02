d32ffa28c8f2628597f3359930f69abd87b5e3d9
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-02 20:54:20 +0700

    Update prime_check.py

--- prime_check.py
+++ prime_check.py
@@ -1,4 +1,4 @@
-def check_prime(n: int):
+def check_prime_int64(n: int):
     if n < 2:
         return False
     assert n < 3825123056546413051  # references https://oeis.org/A014233
@@ -27,3 +27,32 @@ def check_prime(n: int):
 
     return True
 
+
+def check_prime_int32(n: int):
+    if n < 2:
+        return False
+    assert n < 4759123141  # references https://oeis.org/A014233
+    jp = [2, 7, 61]
+    for p in jp:
+        if n % p == 0:
+            return n == p
+
+    r = n - 1
+    e: int = 0
+    while ~r & 1:
+        r >>= 1
+        e = e + 1
+
+    for p in jp:
+        x = pow(p, r, n)
+        t = 0
+        while t < e and x > 1:
+            y: int = x * x % n
+            if y == 1 and x != n - 1:
+                return False
+            x = y
+            t = t + 1
+        if x != 1:
+            return False
+
+    return True
