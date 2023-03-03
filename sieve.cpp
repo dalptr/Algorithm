7b2d1e73ce49032a4644039a37069cdfba9ef78a
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-03 14:14:53 +0700

    Create sieve.cpp

---
+++ sieve.cpp
@@ -0,0 +1,33 @@
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    prime.assign(maximum + 1, true);
+    prime[0] = prime[1] = false;
+    primes = {};
+
+    for (int i = 2; i <= maximum; i += 2) {
+        prime[i] = i == 2;
+        smallest_factor[i] = 2;
+    }
+
+    for (int p = 3; p * p <= maximum; p += 2)
+        if (prime[p])
+            for (int i = p * p; i <= maximum; i += 2 * p)
+                if (prime[i]) {
+                    prime[i] = false;
+                    smallest_factor[i] = p;
+                }
+
+    if (maximum >= 2)
+        primes.push_back(2);
+
+    for (int p = 3; p <= maximum; p += 2)
+        if (prime[p]) {
+            smallest_factor[p] = p;
+            primes.push_back(p);
+        }
+}
