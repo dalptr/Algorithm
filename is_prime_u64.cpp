f6b867c9d53b2759b09fe5f9e94a11f73e5d1d39
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-03 13:47:04 +0700

    Create is_prime_u64.cpp

---
+++ is_prime_u64.cpp
@@ -0,0 +1,33 @@
+const uint64_t tests[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+uint64_t pow_64(__uint128_t a, uint64_t t, uint64_t mod) {
+    uint64_t r = 1;
+    for (; t; t >>= 1, a = a * a % mod) {
+        if (t & 1) {
+            r = __uint128_t(r) * a % mod;
+        }
+    }
+    return r;
+}
+
+bool is_prime(uint64_t n) {
+    if (n == 1) return false;
+    for (uint64_t num: tests) {
+        if (n % num == 0) return n == num;
+    }
+    uint64_t r = n - 1, x, y;
+    int e = 0;
+    while (~r & 1) {
+        r >>= 1, ++e;
+    }
+    for (uint64_t num: tests) {
+        x = pow_64(num, r, n);
+        for (int t = 0; t < e && x > 1; ++t) {
+            y = (__uint128_t) x * x % n;
+            if (y == 1 && x != n - 1) return false;
+            x = y;
+        }
+        if (x != 1) return false;
+    }
+    return true;
+}
