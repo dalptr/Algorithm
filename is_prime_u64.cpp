e719aac91fb6042c6896996caa5ee8e49f575dc9
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-05 23:28:55 +0700

    Update is_prime_u64.cpp

--- is_prime_u64.cpp
+++ is_prime_u64.cpp
@@ -1,7 +1,8 @@
-const uint64_t tests[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+typedef uint64_t u64;
+const u64 candidates[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
 
-uint64_t pow_64(__uint128_t base, uint64_t exp, uint64_t modulo) {
-    uint64_t res = 1;
+u64 pow_64(__uint128_t base, u64 exp, u64 modulo) {
+    u64 res = 1;
     for (; exp; exp >>= 1, base = base * base % modulo) {
         if (exp & 1) {
             res = __uint128_t(res) * base % modulo;
@@ -10,18 +11,18 @@ uint64_t pow_64(__uint128_t base, uint64_t exp, uint64_t modulo) {
     return res;
 }
 
-bool is_prime(uint64_t n) {
+bool is_prime(u64 n) {
     if (n == 1) return false;
-    for (uint64_t num: tests) {
+    for (u64 num: candidates) {
         if (n % num == 0) return n == num;
     }
-    uint64_t r = n - 1, x, y;
+    u64 r = n - 1, x, y;
     int e = 0;
     while (~r & 1) {
         r >>= 1, ++e;
     }
-    for (uint64_t num: tests) {
-        x = pow_64(num, r, n);
+    for (u64 candidate: candidates) {
+        x = pow_64(candidate, r, n);
         for (int t = 0; t < e && x > 1; ++t) {
             y = (__uint128_t) x * x % n;
             if (y == 1 && x != n - 1) return false;
