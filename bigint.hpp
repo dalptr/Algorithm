4877fb66339db68b64ffe1f7cea2369263ec0556
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-06 23:35:26 +0700

    Update bigint.hpp

--- bigint.hpp
+++ bigint.hpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-using i64 = long long;
+using i64 = long long int;
+using u64 = unsigned long long int;
 using u32 = unsigned int;
 const int base = 1e9, base_digits = 9;
 #define sz(x) (x).size()
@@ -66,9 +67,10 @@ struct INTEGER {
 
     INTEGER &operator-=(const INTEGER &other) {
         if (sign == other.sign) {
+            const u32 n = other.z.size();
             if ((sign == 1 && *this >= other) || (sign == -1 && *this <= other)) {
-                for (auto i = 0, carry = 0; i < sz(other.z) || carry; ++i) {
-                    z[i] -= carry + (i < sz(other.z) ? other.z[i] : 0);
+                for (u32 i = 0, carry = 0; i < n || carry; ++i) {
+                    z[i] -= carry + (i < n ? other.z[i] : 0);
                     carry = z[i] < 0;
                     if (carry) z[i] += base;
                 }
@@ -77,7 +79,9 @@ struct INTEGER {
                 *this = other - *this;
                 this->sign = -this->sign;
             }
-        } else *this += -other;
+        } else {
+            *this += -other;
+        }
         return *this;
     }
 
@@ -87,8 +91,10 @@ struct INTEGER {
 
     INTEGER &operator*=(int v) {
         if (v < 0) sign = -sign, v = -v;
-        for (int i = 0, carry = 0; i < sz(z) || carry; ++i) {
-            if (i == sz(z)) z.push_back(0);
+        for (u64 i = 0, carry = 0; i < z.size() || carry; ++i) {
+            if (i == z.size()) {
+                z.push_back(0);
+            }
             i64 cur = (i64) z[i] * v + carry;
             carry = cur / base;
             z[i] = cur % base;
@@ -104,12 +110,12 @@ struct INTEGER {
     friend pair<INTEGER, INTEGER> div_mod(const INTEGER &a1, const INTEGER &b1) {
         int norm = base / (b1.z.back() + 1);
         INTEGER a = a1.abs() * norm, b = b1.abs() * norm, q, r; // make last element of b big
-        q.z.resize(sz(a.z));
-        R0F(i, sz(a.z)) {
+        q.z.resize(a.z.size());
+        for (int i = a.z.size() - 1; i >= 0; --i) {
             r *= base;
             r += a.z[i];
-            int s1 = sz(b.z) < sz(r.z) ? r.z[sz(b.z)] : 0;
-            int s2 = sz(b.z) - 1 < sz(r.z) ? r.z[sz(b.z) - 1] : 0;
+            int s1 = (b.z.size() < r.z.size() ? r.z[b.z.size()] : 0);
+            int s2 = (b.z.size() - 1 < r.z.size() ? r.z[b.z.size() - 1] : 0);
             int d = ((i64) s1 * base + s2) / b.z.back(); // best approximation
             r -= b * d;
             while (r < 0) r += b, --d;
@@ -122,13 +128,17 @@ struct INTEGER {
         return {q, r / norm};
     }
 
-    INTEGER operator/(const INTEGER &v) const { return div_mod(*this, v).first; }
+    INTEGER operator/(const INTEGER &v) const {
+        return div_mod(*this, v).first;
+    }
 
-    INTEGER operator%(const INTEGER &v) const { return div_mod(*this, v).second; }
+    INTEGER operator%(const INTEGER &v) const {
+        return div_mod(*this, v).second;
+    }
 
     INTEGER &operator/=(int v) {
         if (v < 0) sign = -sign, v = -v;
-        for (int i = sz(z) - 1, rem = 0; i >= 0; --i) {
+        for (int i = z.size() - 1, rem = 0; i >= 0; --i) {
             i64 cur = z[i] + rem * (i64) base;
             z[i] = cur / v;
             rem = cur % v;
@@ -137,35 +147,59 @@ struct INTEGER {
         return *this;
     }
 
-    INTEGER operator/(int v) const { return INTEGER(*this) /= v; }
+    INTEGER operator/(int v) const {
+        return INTEGER(*this) /= v;
+    }
 
     int operator%(int v) const {
         if (v < 0) v = -v;
         int m = 0;
-        R0F(i, sz(z)) m = (z[i] + m * (i64) base) % v;
+        for (int i = z.size() - 1; i >= 0; --i) {
+            m = (z[i] + m * (i64) base) % v;
+        }
         return m * sign;
     }
 
-    INTEGER &operator*=(const INTEGER &v) { return *this = *this * v; }
-
-    INTEGER &operator/=(const INTEGER &v) { return *this = *this / v; }
-
-    bool operator<(const INTEGER &v) const {
-        if (sign != v.sign) return sign < v.sign;
-        if (sz(z) != sz(v.z)) return sz(z) * sign < sz(v.z) * v.sign;
-        R0F(i, sz(z)) if (z[i] != v.z[i]) return z[i] * sign < v.z[i] * sign;
-        return 0; // equal
+    INTEGER &operator*=(const INTEGER &v) {
+        return *this = *this * v;
     }
 
-    bool operator>(const INTEGER &v) const { return v < *this; }
+    INTEGER &operator/=(const INTEGER &v) {
+        return *this = *this / v;
+    }
 
-    bool operator<=(const INTEGER &v) const { return !(v < *this); }
+    bool operator<(const INTEGER &v) const {
+        if (sign != v.sign) {
+            return sign < v.sign;
+        }
+        if (z.size() != v.z.size()) {
+            return sz(z) * sign < sz(v.z) * v.sign;
+        }
+        for (int i = z.size() - 1; i >= 0; --i) {
+            if (z[i] != v.z[i]) return z[i] * sign < v.z[i] * sign;
+        }
+        return false;
+    }
 
-    bool operator>=(const INTEGER &v) const { return !(*this < v); }
+    bool operator>(const INTEGER &v) const {
+        return v < *this;
+    }
 
-    bool operator==(const INTEGER &v) const { return !(*this < v) && !(v < *this); }
+    bool operator<=(const INTEGER &v) const {
+        return !(v < *this);
+    }
 
-    bool operator!=(const INTEGER &v) const { return *this < v || v < *this; }
+    bool operator>=(const INTEGER &v) const {
+        return !(*this < v);
+    }
+
+    bool operator==(const INTEGER &v) const {
+        return !(*this < v) && !(v < *this);
+    }
+
+    bool operator!=(const INTEGER &v) const {
+        return *this < v || v < *this;
+    }
 
     void trim() {
         while (!z.empty() && z.back() == 0) {
@@ -187,7 +221,9 @@ struct INTEGER {
 
     [[maybe_unused]] i64 to_long() const {
         i64 res = 0;
-        R0F(i, sz(z)) res = res * base + z[i];
+        for (int i = z.size() - 1; i >= 0; --i) {
+            res = res * base + z[i];
+        }
         return res * sign;
     }
 
@@ -226,15 +262,19 @@ struct INTEGER {
 
     friend ostream &operator<<(ostream &os, const INTEGER &v) {
         if (v.sign == -1) os << '-';
-        os << (!sz(v.z) ? 0 : v.z.back());
-        R0F(i, sz(v.z) - 1) os << setw(base_digits) << setfill('0') << v.z[i];
+        os << (v.z.empty() ? 0 : v.z.back());
+        for (int i = v.z.size() - 2; i >= 0; --i) {
+            os << setw(base_digits) << setfill('0') << v.z[i];
+        }
         return os; // pad with zeroes
     }
 
     static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits) {
         vector<long long> p(max(old_digits, new_digits) + 1); // blocks of 10^{old} -> 10^{new}
         p[0] = 1;
-        FOR(i, 1, sz(p)) p[i] = p[i - 1] * 10;
+        for (u64 i = 1; i < p.size(); ++i) {
+            p[i] = p[i - 1] * 10;
+        }
         vector<int> res;
         i64 cur = 0;
         int cur_digits = 0;
@@ -252,29 +292,45 @@ struct INTEGER {
         return res;
     }
 
-    static vector<long long> karatMul(const vector<long long> &a, const vector<long long> &b) { // karatsuba
-        int n = sz(a);
+    static vector<long long> karatsuba_multiple(const vector<i64> &a, const vector<i64> &b) {
+        const int n = a.size();
         vector<long long> res(2 * n);
         if (n <= 32) {
-            F0R(i, n) F0R(j, n) res[i + j] += a[i] * b[j];
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < (n); ++j) {
+                    res[i + j] += a[i] * b[j];
+                }
+            }
             return res;
         }
         int k = n / 2;
         vector<long long> a1(begin(a), begin(a) + k), a2(k + all(a));
         vector<long long> b1(begin(b), begin(b) + k), b2(k + all(b));
-        vector<long long> a1b1 = karatMul(a1, b1), a2b2 = karatMul(a2, b2);
-        F0R(i, k) a2[i] += a1[i], b2[i] += b1[i];
-        vector<long long> r = karatMul(a2, b2); // three instead of four products
-        F0R(i, sz(a1b1)) r[i] -= a1b1[i];
-        F0R(i, sz(a2b2)) r[i] -= a2b2[i];
-        F0R(i, sz(r)) res[i + k] += r[i];
-        F0R(i, sz(a1b1)) res[i] += a1b1[i];
-        F0R(i, sz(a2b2)) res[i + n] += a2b2[i];
+        vector<long long> a1b1 = karatsuba_multiple(a1, b1), a2b2 = karatsuba_multiple(a2, b2);
+        for (int i = 0; i < k; ++i) {
+            a2[i] += a1[i], b2[i] += b1[i];
+        }
+        vector<long long> r = karatsuba_multiple(a2, b2);
+        for (u64 i = 0; i < a1b1.size(); ++i) {
+            r[i] -= a1b1[i];
+        }
+        for (u64 i = 0; i < a2b2.size(); ++i) {
+            r[i] -= a2b2[i];
+        }
+        F0R(i, sz(r)) {
+            res[i + k] += r[i];
+        }
+        F0R(i, sz(a1b1)) {
+            res[i] += a1b1[i];
+        }
+        F0R(i, sz(a2b2)) {
+            res[i + n] += a2b2[i];
+        }
         return res;
     }
 
     INTEGER operator*(const INTEGER &v) const {
-        if (min(sz(z), sz(v.z)) < 150) return mul_simple(v);
+        if (min(sz(z), sz(v.z)) < 150) return native_multiple(v);
         INTEGER res;
         res.sign = sign * v.sign; // should work as long as # of digits isn't too large (> i64ONG_MAX/10^{12})
         vector<int> a6 = convert_base(this->z, base_digits, 6); // blocks of 10^6 instead of 10^9
@@ -283,7 +339,7 @@ struct INTEGER {
         while (sz(a) < sz(b)) a.push_back(0);
         while (sz(b) < sz(a)) b.push_back(0);
         while (sz(a) & (sz(a) - 1)) a.push_back(0), b.push_back(0); // make size power of 2
-        vector<long long> c = karatMul(a, b);
+        vector<long long> c = karatsuba_multiple(a, b);
         i64 cur = 0;
         for (auto i = 0; i < c.size(); ++i) { // process carries
             cur += c[i];
@@ -295,7 +351,7 @@ struct INTEGER {
         return res;
     }
 
-    INTEGER mul_simple(const INTEGER &v) const {
+    INTEGER native_multiple(const INTEGER &v) const {
         INTEGER res;
         res.sign = sign * v.sign;
         res.z.resize(sz(z) + sz(v.z));
@@ -311,6 +367,14 @@ struct INTEGER {
         return res;
     }
 
+    INTEGER random(u32 digits) {
+        string s;
+        for (u32 i = 0; i < digits; ++i) {
+            s += rand() % 10 + '0';
+        }
+        return INTEGER(s);
+    }
+
     friend string to_string(const INTEGER &v) {
         stringstream ss;
         ss << v;
@@ -320,8 +384,3 @@ struct INTEGER {
     }
 };
 
-INTEGER random_INTERGER(int n) {
-    string s;
-    F0R(i, n) s += rand() % 10 + '0';
-    return INTEGER(s);
-}
