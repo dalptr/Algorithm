8e43c32bfccde26633d15168440ed25a04ccebd7
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-09 19:21:28 +0700

    Update bigint.hpp

--- bigint.hpp
+++ bigint.hpp
@@ -1,30 +1,22 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
+using i64 = long long int;
+using u64 = unsigned long long int;
+using u32 = unsigned int;
 constexpr int base = 1e9, base_digits = 9;
 constexpr int negative = -1;
 constexpr int positive = 1;
 constexpr int prime_candidates[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41};
-struct INTEGER {
-#ifndef DEVELOPMENT_MODE
-#define assert(...)
-#endif
-#define all(x) begin(x), end(x)
-#define sz(x) (x).size()
-    using i64 = long long int;
-    using u64 = unsigned long long int;
-    using u32 = unsigned int;
-private:
 
+struct INTEGER {
 public:
     vector<int> z;
     int sign;
+
     INTEGER() : sign(1) {}
 
-    INTEGER(i64 v) {
-        *this = v;
-    }
+    INTEGER(i64 v) { *this = v; }
 
     INTEGER &operator=(i64 v) { // verify
         sign = (v < 0 ? negative : positive);
@@ -37,9 +29,7 @@ public:
         return *this;
     }
 
-    INTEGER(const string &s) {
-        read(s);
-    }
+    INTEGER(const string &s) { read(s); }
 
     INTEGER &operator+=(const INTEGER &other) {
         if (sign == other.sign) {
@@ -60,9 +50,7 @@ public:
         return *this;
     }
 
-    friend INTEGER operator+(INTEGER a, const INTEGER &b) {
-        return a += b;
-    }
+    friend INTEGER operator+(INTEGER a, const INTEGER &b) { return a += b; }
 
     INTEGER &operator-=(const INTEGER &other) {
         if (sign == other.sign) {
@@ -84,9 +72,7 @@ public:
         return *this;
     }
 
-    friend INTEGER operator-(INTEGER a, const INTEGER &b) {
-        return a -= b;
-    }
+    friend INTEGER operator-(INTEGER a, const INTEGER &b) { return a -= b; }
 
     INTEGER &operator*=(int v) {
         if (v < 0) sign = -sign, v = -v;
@@ -102,9 +88,7 @@ public:
         return *this;
     }
 
-    INTEGER operator*(int v) const {
-        return INTEGER(*this) *= v;
-    }
+    INTEGER operator*(int v) const { return INTEGER(*this) *= v; }
 
     friend pair<INTEGER, INTEGER> div_mod(const INTEGER &a1, const INTEGER &b1) {
         int norm = base / (b1.z.back() + 1);
@@ -127,13 +111,9 @@ public:
         return {q, r / norm};
     }
 
-    INTEGER operator/(const INTEGER &v) const {
-        return div_mod(*this, v).first;
-    }
+    INTEGER operator/(const INTEGER &v) const { return div_mod(*this, v).first; }
 
-    INTEGER operator%(const INTEGER &v) const {
-        return div_mod(*this, v).second;
-    }
+    INTEGER operator%(const INTEGER &v) const { return div_mod(*this, v).second; }
 
     INTEGER &operator/=(int v) {
         if (v < 0) sign = -sign, v = -v;
@@ -146,70 +126,42 @@ public:
         return *this;
     }
 
-    INTEGER operator/(int v) const {
-        return INTEGER(*this) /= v;
-    }
+    INTEGER operator/(int v) const { return INTEGER(*this) /= v; }
 
     int operator%(int v) const {
         if (v < 0) v = -v;
         int m = 0;
-        for (int i = z.size() - 1; i >= 0; --i) {
-            m = (z[i] + m * (i64) base) % v;
-        }
+        for (int i = z.size() - 1; i >= 0; --i) m = (z[i] + m * (i64) base) % v;
         return m * sign;
     }
 
-    INTEGER &operator*=(const INTEGER &v) {
-        return *this = *this * v;
-    }
+    INTEGER &operator*=(const INTEGER &v) { return *this = *this * v; }
 
-    INTEGER &operator/=(const INTEGER &v) {
-        return *this = *this / v;
-    }
+    INTEGER &operator/=(const INTEGER &v) { return *this = *this / v; }
 
     bool operator<(const INTEGER &v) const {
-        if (sign != v.sign) {
-            return sign < v.sign;
-        }
-        if (z.size() != v.z.size()) {
-            return int(z.size()) * sign < int(v.z.size()) * v.sign;
-        }
-        for (int i = z.size() - 1; i >= 0; --i) {
-            if (z[i] != v.z[i]) return z[i] * sign < v.z[i] * sign;
-        }
+        if (sign != v.sign) return sign < v.sign;
+        if (z.size() != v.z.size()) return int(z.size()) * sign < int(v.z.size()) * v.sign;
+        for (int i = z.size() - 1; i >= 0; --i) if (z[i] != v.z[i]) return z[i] * sign < v.z[i] * sign;
         return false;
     }
 
-    bool operator>(const INTEGER &v) const {
-        return v < *this;
-    }
+    bool operator>(const INTEGER &v) const { return v < *this; }
 
-    bool operator<=(const INTEGER &v) const {
-        return !(v < *this);
-    }
+    bool operator<=(const INTEGER &v) const { return !(v < *this); }
 
-    bool operator>=(const INTEGER &v) const {
-        return !(*this < v);
-    }
+    bool operator>=(const INTEGER &v) const { return !(*this < v); }
 
-    bool operator==(const INTEGER &v) const {
-        return !(*this < v) && !(v < *this);
-    }
+    bool operator==(const INTEGER &v) const { return !(*this < v) && !(v < *this); }
 
-    bool operator!=(const INTEGER &v) const {
-        return *this < v || v < *this;
-    }
+    bool operator!=(const INTEGER &v) const { return *this < v || v < *this; }
 
     void trim() {
-        while (!z.empty() && z.back() == 0) {
-            z.pop_back();
-        }
+        while (!z.empty() && z.back() == 0) z.pop_back();
         if (z.empty()) sign = positive;
     }
 
-    bool is_zero() const {
-        return z.empty();
-    }
+    bool is_zero() const { return z.empty(); }
 
     friend INTEGER operator-(INTEGER v) {
         if (!v.z.empty()) v.sign = -v.sign;
@@ -220,19 +172,13 @@ public:
 
     [[maybe_unused]] i64 to_long() const {
         i64 res = 0;
-        for (int i = z.size() - 1; i >= 0; --i) {
-            res = res * base + z[i];
-        }
+        for (int i = z.size() - 1; i >= 0; --i) res = res * base + z[i];
         return res * sign;
     }
 
-    friend INTEGER gcd(const INTEGER &a, const INTEGER &b) {
-        return b.is_zero() ? a : gcd(b, a % b);
-    }
+    friend INTEGER gcd(const INTEGER &a, const INTEGER &b) { return b.is_zero() ? a : gcd(b, a % b); }
 
-    [[maybe_unused]] friend INTEGER lcm(const INTEGER &a, const INTEGER &b) {
-        return a / gcd(a, b) * b;
-    }
+    [[maybe_unused]] friend INTEGER lcm(const INTEGER &a, const INTEGER &b) { return a / gcd(a, b) * b; }
 
     void read(const string &s) {
         sign = positive;
@@ -245,8 +191,7 @@ public:
         }
         for (int i = n - 1; i >= pos; i -= base_digits) {
             int x = 0;
-            for (int j = max(pos, i - base_digits + 1); j <= i; j++)
-                x = x * 10 + s[j] - '0';
+            for (int j = max(pos, i - base_digits + 1); j <= i; j++) x = x * 10 + s[j] - '0';
             z.push_back(x);
         }
         trim();
@@ -262,18 +207,14 @@ public:
     friend ostream &operator<<(ostream &os, const INTEGER &v) {
         if (v.sign == -1) os << '-';
         os << (v.z.empty() ? 0 : v.z.back());
-        for (int i = v.z.size() - 2; i >= 0; --i) {
-            os << setw(base_digits) << setfill('0') << v.z[i];
-        }
+        for (int i = v.z.size() - 2; i >= 0; --i) os << setw(base_digits) << setfill('0') << v.z[i];
         return os; // pad with zeroes
     }
 
     static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits) {
         vector<long long> p(max(old_digits, new_digits) + 1); // blocks of 10^{old} -> 10^{new}
         p[0] = 1;
-        for (u64 i = 1; i < p.size(); ++i) {
-            p[i] = p[i - 1] * 10;
-        }
+        for (u64 i = 1; i < p.size(); ++i) p[i] = p[i - 1] * 10;
         vector<int> res;
         i64 cur = 0;
         int cur_digits = 0;
@@ -287,9 +228,7 @@ public:
             }
         }
         res.push_back(cur);
-        while (!res.empty() && res.back() == 0) {
-            res.pop_back();
-        }
+        while (!res.empty() && res.back() == 0) res.pop_back();
         return res;
     }
 
@@ -297,51 +236,35 @@ public:
         const int n = a.size();
         vector<long long> res(2 * n);
         if (n <= 32) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < (n); ++j) {
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < (n); ++j)
                     res[i + j] += a[i] * b[j];
-                }
-            }
             return res;
         }
         int k = n / 2;
-        vector<long long> a1(begin(a), begin(a) + k), a2(k + all(a));
-        vector<long long> b1(begin(b), begin(b) + k), b2(k + all(b));
+        vector<long long> a1(begin(a), begin(a) + k), a2(k + begin(a), end(a));
+        vector<long long> b1(begin(b), begin(b) + k), b2(k + begin(b), end(b));
         vector<long long> a1b1 = karatsuba_multiple(a1, b1), a2b2 = karatsuba_multiple(a2, b2);
-        for (int i = 0; i < k; ++i) {
-            a2[i] += a1[i], b2[i] += b1[i];
-        }
+        for (int i = 0; i < k; ++i) a2[i] += a1[i], b2[i] += b1[i];
         vector<long long> r = karatsuba_multiple(a2, b2);
-        for (u64 i = 0; i < a1b1.size(); ++i) {
-            r[i] -= a1b1[i];
-        }
-        for (u64 i = 0; i < a2b2.size(); ++i) {
-            r[i] -= a2b2[i];
-        }
-        for (int i = (0); i < ((r).size()); ++i) {
-            res[i + k] += r[i];
-        }
-        for (int i = (0); i < ((a1b1).size()); ++i) {
-            res[i] += a1b1[i];
-        }
-        for (int i = (0); i < ((a2b2).size()); ++i) {
-            res[i + n] += a2b2[i];
-        }
+        for (u64 i = 0; i < a1b1.size(); ++i) r[i] -= a1b1[i];
+        for (u64 i = 0; i < a2b2.size(); ++i) r[i] -= a2b2[i];
+        for (int i = (0); i < ((r).size()); ++i) res[i + k] += r[i];
+        for (int i = (0); i < ((a1b1).size()); ++i) res[i] += a1b1[i];
+        for (int i = (0); i < ((a2b2).size()); ++i) res[i + n] += a2b2[i];
         return res;
     }
 
     INTEGER operator*(const INTEGER &v) const {
-        if (min(z.size(), v.z.size()) < 150) {
-            return native_multiple(v);
-        }
+        if (min(z.size(), v.z.size()) < 150) return native_multiple(v);
         INTEGER res;
         res.sign = sign * v.sign;
         vector<int> a6 = convert_base(this->z, base_digits, 6);
         vector<int> b6 = convert_base(v.z, base_digits, 6);
-        vector<long long> a(all(a6)), b(all(b6));
-        while (sz(a) < sz(b)) a.push_back(0);
-        while (sz(b) < sz(a)) b.push_back(0);
-        while (sz(a) & (sz(a) - 1)) a.push_back(0), b.push_back(0); // make size power of 2
+        vector<long long> a(begin(a6), end(a6)), b(begin(b6), end(b6));
+        while (a.size() < b.size()) a.push_back(0);
+        while (b.size() < a.size()) b.push_back(0);
+        while (a.size() & (a.size() - 1)) a.push_back(0), b.push_back(0); // make size power of 2
         vector<long long> c = karatsuba_multiple(a, b);
         i64 cur = 0;
         for (auto i = 0; i < c.size(); ++i) { // process carries
@@ -373,9 +296,7 @@ public:
 
     [[maybe_unused]] INTEGER random(u32 digits) {
         string s;
-        for (u32 i = 0; i < digits; ++i) {
-            s += rand() % 10 + '0';
-        }
+        for (u32 i = 0; i < digits; ++i) s += rand() % 10 + '0';
         return INTEGER(s);
     }
 
@@ -409,13 +330,12 @@ public:
         }
         INTEGER r = *this - 1;
         int e = 0;
-        while (r % 2 == 0) {
+        while (r.z[0] % 2 == 0) { // r is even
             r /= 2;
             ++e;
         }
         for (int prime: prime_candidates) {
             INTEGER x = power_modulo(prime, r, *this);
-            cout << x << endl;
             for (int t = 0; t < e && x > 1; ++t) {
                 INTEGER y = x * x;
                 y = y % *this;
