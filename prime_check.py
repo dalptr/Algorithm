1ade6d01744be444dea5e5fe9982fed39b1b98b6
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-03-11 14:58:11 +0700

    Update prime_check.py

--- prime_check.py
+++ prime_check.py
@@ -1,15 +1,13 @@
-maxN: int = int(1e24)
-tests: list[int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+candidates: list[int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
 
 
 def isPrime(n: int) -> bool:
     if n < 2:
         return False
-    assert n < maxN
 
-    for p in tests:
-        if n % p == 0:
-            return n == p
+    for prime in candidates:
+        if n % prime == 0:
+            return n == prime
 
     r: int = n - 1
     e: int = 0
@@ -17,17 +15,16 @@ def isPrime(n: int) -> bool:
         r >>= 1
         e = e + 1
 
-    for p in tests:
-        x: int = pow(p, r, n)
-        t: int = 0
-        while t < e and x > 1:
+    for prime in candidates:
+        x: int = pow(prime, r, n)
+        for t in range(e):
+            if x <= 1:
+                break
             y: int = x * x % n
             if y == 1 and x != n - 1:
                 return False
             x = y
-            t = t + 1
         if x != 1:
             return False
 
     return True
-
