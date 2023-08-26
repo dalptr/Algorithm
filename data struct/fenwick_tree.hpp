5b7f6c8330096b1162a5a2a950bcde0c939188f3
Nguyen Trung <cvb000x00@gmail.com>
2023-08-26 14:44:52 +0700

    add attribute unused

--- data struct/fenwick_tree.hpp
+++ data struct/fenwick_tree.hpp
@@ -1,19 +1,21 @@
-struct FenwickTree {
+#include <bits/stdc++.h>
+
+struct __attribute__((unused)) FenwickTree {
     using type = long long;
     std::vector<type> bit;
 
-    FenwickTree(size_t size) {
+    __attribute__((unused)) explicit FenwickTree(size_t size) {
         bit.resize(size + 1, 0);
     }
 
-    void update(size_t pos, type val) {
+    __attribute__((unused)) void update(size_t pos, type val) {
         while (pos < bit.size()) {
             bit[pos] += val;
             pos += pos & -pos;
         }
     }
 
-    type query(type pos) {
+    __attribute__((unused)) type query(type pos) {
         type sum = 0;
         while (pos > 0) {
             sum += bit[pos];
