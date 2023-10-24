079e6f566b4f407b20b0100979f4383a182b5503
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-10-25 03:30:58 +0700

    Update index_storage.cpp

--- data struct/index_storage.cpp
+++ data struct/index_storage.cpp
@@ -1,6 +1,6 @@
 struct [[maybe_unused]] IndexStorage {
     static constexpr int MEMSET_VAL = 0x3f;
-    static constexpr int DEFAULT_VALUE = 0x3f3f3f3f; 
+    static constexpr int DEFAULT_VALUE = 0x3f3f3f3f;
     static constexpr int MAX_NUM = int(1e5);
     static constexpr int MIN_NUM = MAX_NUM * -1;
     static constexpr int BUFFER_SIZE = MAX_NUM * 2 + 1;
@@ -10,10 +10,10 @@ struct [[maybe_unused]] IndexStorage {
         return index + MAX_NUM;
     }
 
-    [[maybe_unused]] explicit IndexStorage(const std::vector<int> &numbers) {
+    [[maybe_unused]] explicit IndexStorage(int *begin, const int *end) {
         memset(buffer, MEMSET_VAL, sizeof(buffer));
-        for (int i = 0, n = (int) numbers.size(); i < n; ++i) {
-            buffer[get_unsigned_index(numbers[i])] = i;
+        for (int *i = begin, index = 0; i != end; ++i, ++index) {
+            buffer[get_unsigned_index(*i)] = index;
         }
     }
 
