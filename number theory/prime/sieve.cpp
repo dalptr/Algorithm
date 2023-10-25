0288e06577eb3916d022bdfeab340730c414ef64
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-10-25 21:58:58 +0700

    Update sieve.cpp

--- number theory/prime/sieve.cpp
+++ number theory/prime/sieve.cpp
@@ -1,26 +1,24 @@
-#include<vector>
-
 std::vector<int> smallest_factor;
-std::vector<bool> prime;
+std::vector<bool> is_prime;
 std::vector<int> primes;
 
 __attribute__((unused)) void sieve(int maximum) {
     maximum = std::max(maximum, 1);
     smallest_factor.assign(maximum + 1, 0);
-    prime.assign(maximum + 1, true);
-    prime[0] = prime[1] = false;
+    is_prime.assign(maximum + 1, true);
+    is_prime[0] = is_prime[1] = false;
     primes = {};
 
     for (int i = 2; i <= maximum; i += 2) {
-        prime[i] = i == 2;
+        is_prime[i] = i == 2;
         smallest_factor[i] = 2;
     }
 
     for (int p = 3; p * p <= maximum; p += 2)
-        if (prime[p])
+        if (is_prime[p])
             for (int i = p * p; i <= maximum; i += 2 * p)
-                if (prime[i]) {
-                    prime[i] = false;
+                if (is_prime[i]) {
+                    is_prime[i] = false;
                     smallest_factor[i] = p;
                 }
 
@@ -28,7 +26,7 @@ __attribute__((unused)) void sieve(int maximum) {
         primes.push_back(2);
 
     for (int p = 3; p <= maximum; p += 2)
-        if (prime[p]) {
+        if (is_prime[p]) {
             smallest_factor[p] = p;
             primes.push_back(p);
         }
