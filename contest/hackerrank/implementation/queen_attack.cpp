a149a7031b3be2f72aea7777a44755c53e8a6266
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-10-27 01:30:10 +0700

    Update queen_attack.cpp

--- contest/hackerrank/implementation/queen_attack.cpp
+++ contest/hackerrank/implementation/queen_attack.cpp
@@ -1,28 +1,13 @@
 #include <map>
 #include <iostream>
 
-using namespace std;
-
-map<pair<int, int>, bool> is_obstacle;
+std::map<std::pair<int, int>, bool> is_obstacle;
 
 int ans, board_size, n_obstacles;
 int queen_r, queen_c;
 
 int is_valid_position(int r, int c) {
     return (r <= board_size && r >= 1 && c <= board_size && c >= 1);
-}
-
-void move(const int row_step, const int col_step) {
-    int r = queen_r + row_step;
-    int c = queen_c + col_step;
-    while (is_valid_position(r, c)) {
-        if (is_obstacle[{r, c}]) {
-            break;
-        }
-        r += row_step;
-        c += col_step;
-        ++ans;
-    }
 }
 
 struct direction {
@@ -41,17 +26,30 @@ constexpr direction QUEEN_DIRECTIONS[] = {
         {-1, -1}
 };
 
+void move(direction direction) {
+    int r = queen_r + direction.row_step;
+    int c = queen_c + direction.col_step;
+    while (is_valid_position(r, c)) {
+        if (is_obstacle[{r, c}]) {
+            break;
+        }
+        r += direction.row_step;
+        c += direction.col_step;
+        ++ans;
+    }
+}
+
 int main() {
-    cin >> board_size >> n_obstacles;
-    cin >> queen_r >> queen_c;
+    std::cin >> board_size >> n_obstacles;
+    std::cin >> queen_r >> queen_c;
     while (n_obstacles--) {
         int obstacle_r, obstacle_c;
-        cin >> obstacle_r >> obstacle_c;
+        std::cin >> obstacle_r >> obstacle_c;
         is_obstacle[{obstacle_r, obstacle_c}] = true;
     }
     for (auto &direction: QUEEN_DIRECTIONS) {
-        move(direction.row_step, direction.col_step);
+        move(direction);
     }
-    cout << ans;
+    std::cout << ans;
     return 0;
 }
