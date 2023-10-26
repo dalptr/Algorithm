c36c5eb67ad72481b6cc223954437c9e2c92ddae
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-10-27 01:21:24 +0700

    Update queen_attack.cpp

--- contest/hackerrank/implementation/queen_attack.cpp
+++ contest/hackerrank/implementation/queen_attack.cpp
@@ -1,21 +1,22 @@
-#include<map>
-#include<iostream>
+#include <map>
+#include <iostream>
 
 using namespace std;
 
-map<pair<int, int>, bool> obstacles;
+map<pair<int, int>, bool> is_obstacle;
 
-int ans, edge, n_obstacles;
+int ans, board_size, n_obstacles;
+int queen_r, queen_c;
 
 int is_valid_position(int r, int c) {
-    return (r <= edge && r > 0 && c <= edge && c > 0);
+    return (r <= board_size && r >= 1 && c <= board_size && c >= 1);
 }
 
-void move(int x, int y, int row_step, int col_step) {
-    x += row_step;
-    y += col_step;
+void move(const int row_step, const int col_step) {
+    int x = queen_r + row_step;
+    int y = queen_c + col_step;
     while (is_valid_position(x, y)) {
-        if (obstacles[{x, y}]) {
+        if (is_obstacle[{x, y}]) {
             break;
         }
         x += row_step;
@@ -29,7 +30,7 @@ struct direction {
     int col_step;
 };
 
-constexpr direction directions[] = {
+constexpr direction QUEEN_DIRECTIONS[] = {
         {1,  0},
         {-1, 0},
         {0,  1},
@@ -41,16 +42,15 @@ constexpr direction directions[] = {
 };
 
 int main() {
-    cin >> edge >> n_obstacles;
-    int queen_r, queen_c;
+    cin >> board_size >> n_obstacles;
     cin >> queen_r >> queen_c;
     while (n_obstacles--) {
         int obstacle_r, obstacle_c;
         cin >> obstacle_r >> obstacle_c;
-        obstacles[{obstacle_r, obstacle_c}] = true;
+        is_obstacle[{obstacle_r, obstacle_c}] = true;
     }
-    for (auto &direction: directions) {
-        move(queen_r, queen_c, direction.row_step, direction.col_step);
+    for (auto &direction: QUEEN_DIRECTIONS) {
+        move(direction.row_step, direction.col_step);
     }
     cout << ans;
     return 0;
