5d50176d29c50f41704b5c83748d57f7c978f9ab
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-10-27 23:51:33 +0700

    Update point.cpp

--- data struct/point.cpp
+++ data struct/point.cpp
@@ -1,42 +1,42 @@
 template<class T>
-struct [[maybe_unused]] Point {
+struct [[maybe_unused]] point {
     T x, y;
 
-    explicit Point(T x = 0, T y = 0) : x(x), y(y) {}
+    explicit point(T x = 0, T y = 0) : x(x), y(y) {}
 
-    bool operator<(Point p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator<(point p) const { return tie(x, y) < tie(p.x, p.y); }
 
-    bool operator==(Point p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator==(point p) const { return tie(x, y) == tie(p.x, p.y); }
 
-    Point operator+(Point p) const { return Point(x + p.x, y + p.y); }
+    point operator+(point p) const { return point(x + p.x, y + p.y); }
 
-    Point operator-(Point p) const { return Point(x - p.x, y - p.y); }
+    point operator-(point p) const { return point(x - p.x, y - p.y); }
 
-    Point operator*(T d) const { return Point(x * d, y * d); }
+    point operator*(T d) const { return point(x * d, y * d); }
 
-    Point operator/(T d) const { return Point(x / d, y / d); }
+    point operator/(T d) const { return point(x / d, y / d); }
 
-    [[maybe_unused]] T dot(Point p) const { return x * p.x + y * p.y; }
+    [[maybe_unused]] T dot(point p) const { return x * p.x + y * p.y; }
 
-    T cross(Point p) const { return x * p.y - y * p.x; }
+    T cross(point p) const { return x * p.y - y * p.x; }
 
-    T cross(Point a, Point b) const { return (a - *this).cross(b - *this); }
+    T cross(point a, point b) const { return (a - *this).cross(b - *this); }
 
     double dist() const { return sqrt((double) (x * x + y * y)); }
 
     [[maybe_unused]] double angle() const { return atan2(y, x); }
 
-    Point unit() const { return *this / dist(); }
+    point unit() const { return *this / dist(); }
 
-    Point perp() const { return Point(-y, x); }
+    point perp() const { return point(-y, x); }
 
-    [[maybe_unused]] Point normal() const { return perp().unit(); }
+    [[maybe_unused]] point normal() const { return perp().unit(); }
 
-    [[maybe_unused]] Point rotate(double a) const {
-        return Point(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
+    [[maybe_unused]] point rotate(double a) const {
+        return point(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
     }
 
-    friend std::ostream &operator<<(std::ostream &os, Point p) {
+    friend std::ostream &operator<<(std::ostream &os, point p) {
         return os << "(" << p.x << "," << p.y << ")";
     }
 };
