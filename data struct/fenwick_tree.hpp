fb9714e844cb56aa78ec923f32956e91d6db13c9
Nguyen Trung <87151674+dalptr@users.noreply.github.com>
2023-11-01 03:07:40 +0700

    Update fenwick_tree.hpp

--- data struct/fenwick_tree.hpp
+++ data struct/fenwick_tree.hpp
@@ -1,26 +1,101 @@
-#include <bits/stdc++.h>
-
-struct __attribute__((unused)) FenwickTree {
-    using type = long long;
-    std::vector<type> bit;
-
-    __attribute__((unused)) explicit FenwickTree(size_t size) {
-        bit.resize(size + 1, 0);
+template<typename T>
+struct [[maybe_unused]] fenwick_tree {
+    static int highest_bit(unsigned x) {
+        return x == 0 ? -1 : 31 - __builtin_clz(x);
     }
 
-    __attribute__((unused)) void update(size_t pos, type val) {
-        while (pos < bit.size()) {
-            bit[pos] += val;
-            pos += pos & -pos;
+    int tree_n = 0;
+    T tree_sum = T();
+    vector<T> tree;
+
+    explicit fenwick_tree(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+
+    void init(int n) {
+        tree_n = n;
+        tree_sum = T();
+        tree.assign(tree_n + 1, T());
+    }
+
+    template<typename T_array>
+    [[maybe_unused]] void build(const T_array &initial) {
+        assert(int(initial.size()) == tree_n);
+        tree_sum = T();
+
+        for (int i = 1; i <= tree_n; i++) {
+            tree[i] = initial[i - 1];
+            tree_sum += initial[i - 1];
+
+            for (int k = (i & -i) >> 1; k > 0; k >>= 1)
+                tree[i] += tree[i - k];
         }
     }
 
-    __attribute__((unused)) type query(type pos) {
-        type sum = 0;
-        while (pos > 0) {
-            sum += bit[pos];
-            pos -= pos & -pos;
-        }
+    void update(int index, const T &change) {
+        assert(0 <= index && index < tree_n);
+        tree_sum += change;
+
+        for (int i = index + 1; i <= tree_n; i += i & -i)
+            tree[i] += change;
+    }
+
+    T query(int count) const {
+        count = min(count, tree_n);
+        T sum = T();
+
+        for (int i = count; i > 0; i -= i & -i)
+            sum += tree[i];
+
         return sum;
+    }
+
+    [[maybe_unused]] T query_suffix(int start) const {
+        return tree_sum - query(start);
+    }
+
+    T query(int a, int b) const {
+        return query(b) - query(a);
+    }
+
+    T get(int a) const {
+        assert(0 <= a && a < tree_n);
+        int above = a + 1;
+        T sum = tree[above];
+        above -= above & -above;
+
+        while (a != above) {
+            sum -= tree[a];
+            a -= a & -a;
+        }
+
+        return sum;
+    }
+
+    [[maybe_unused]] bool set(int index, T value) {
+        assert(0 <= index && index < tree_n);
+        T current = get(index);
+
+        if (current == value)
+            return false;
+
+        update(index, value - current);
+        return true;
+    }
+
+    [[maybe_unused]] int find_last_prefix(T sum) const {
+        if (sum < T())
+            return -1;
+
+        int prefix = 0;
+
+        for (int k = highest_bit(tree_n); k >= 0; k--)
+            if (prefix + (1 << k) <= tree_n && tree[prefix + (1 << k)] <= sum) {
+                prefix += 1 << k;
+                sum -= tree[prefix];
+            }
+
+        return prefix;
     }
 };
